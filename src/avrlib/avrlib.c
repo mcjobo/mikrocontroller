1d85ffa617ef38d93e6680551c31a54ca72c26a8
mcjobo <mcjobo@gmail.com>
2013-10-12 15:27:02 +0200

    added procyon avrlib to the project

---
+++ src/avrlib/avrlib.c
@@ -0,0 +1,15 @@
+/*
+ * avrlib.c
+ *
+ * Created: 10.10.2013 19:21:50
+ *  Author: jbolay
+ */ 
+
+#include <avr/io.h>
+
+int function(void)
+{
+    //TODO:: Please write your application code
+
+    return 0;
+}
