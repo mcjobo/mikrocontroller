a274c03b97187ebcf58204e40a69b04058add992
Micha <sun_and_planet@yahoo.com>
2013-10-19 23:27:06 +0200

    added openmcp project

---
+++ src/openmcp/openmcp.c
@@ -0,0 +1,17 @@
+/*
+ * openmcp.c
+ *
+ * Created: 18.10.2013 22:55:36
+ *  Author: Michael
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
