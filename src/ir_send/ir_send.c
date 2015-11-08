a0122dbb648797ac0f34affe7e66faf51532e19c
Jörg Bolay <jorg@bolay.org>
2015-11-08 19:41:58 +0100

    ir_send vorläufig fertig

--- src/ir_send/ir_send.c
+++ src/ir_send/ir_send.c
@@ -2,24 +2,40 @@
  
 #include "ir_send.h"
 
+//
+// Please modify the following lines. mac and ip have to be unique
+// in your local area network. You can not have the same numbers in
+// two devices:
+//static uint8_t mymac[6] = {0xb4,0x28,0x95,0x23,0x60,0x23};
 
 
+uint8_t second = 0;
+uint8_t minute = 0;
+void everyMinute(){
+
+}
 void everySecond(){
+    ++second;
+    if(second == 60){
+        everyMinute();
+        second = 0;
+    }
 
+    if(second % 6 == 0){
+        dhcp_6sec_tick();
+    }
 }
 
 uint16_t counter = 0;
 void timerFired(){
-	(void) irmp_ISR();
+	(void) irsnd_ISR();
 
 	counter += 1;
 	if(counter >= 15000){
 		everySecond();
         counter = 0;
 		
-	} else if(counter >= 30000){
-		counter = 0;
-	}
+	} 
 }
 
 void blink(uint16_t time){
@@ -60,7 +76,6 @@ void get_ip_from_Dhcp(){
         plen=enc28j60PacketReceive(BUFFER_SIZE, buf);
         buf[BUFFER_SIZE]='\0';
         rval=packetloop_dhcp_initial_ip_assignment(buf,plen,mymac[5]);
-        rprintf("rval: %d\r\n", rval);
     }
     // we have an IP:
     dhcp_get_my_ip(myip,netmask,gwip); 
@@ -94,9 +109,25 @@ void bufferFunction(unsigned char c){
 	++pointer;
 }
 
+uint16_t parseString(char* string, uint8_t start, uint8_t length){
+    char buf[5];
+    memcpy( buf, &string[start], length );
+    uint16_t number = atoi(buf);
+    //rprintfStr(buf);rprintfCRLF();
+    //rprintf("parsed:'%d'%d'%d'\r\n", number, start, length);
+    return number;
+}
+
 int main (void) {
+    uint16_t plen;
+    uint16_t dat_p;
+    uint8_t rval;
+    uint8_t payloadlen=0;
+    IRMP_DATA irmp_data;
 
     INIT_LED;
+
+    
     
     blink(100);
     blink(100);
@@ -106,10 +137,11 @@ int main (void) {
 
     uart_init(UART_BAUD_SELECT(9600,8000000));
     rprintfInit(uart_putc);
-    rprintf("hello running\r\n");
+    rprintf("\r\nhello running\r\n");
 
     // init timer
     timer_init();
+    irsnd_init();                                                           // initialize irsnd
     /*initialize enc28j60*/
     enc28j60Init(mymac);
     _delay_loop_1(0); // 60us
@@ -128,10 +160,52 @@ int main (void) {
 
     blink(200);
     blink(200);
- 	LED_ON;
 
     while(1) {
-		
+        plen=enc28j60PacketReceive(BUFFER_SIZE, buf);
+        plen=packetloop_dhcp_renewhandler(buf,plen); // for this to work you have to call dhcp_6sec_tick() every 6 sec
+        dat_p=packetloop_arp_icmp_tcp(buf,plen);
+        buf[BUFFER_SIZE]='\0';
+
+		// udp start, we listen on udp port 1200=0x4B0
+        // check if ip packets are for us:
+        if(eth_type_is_ip_and_my_ip(buf,plen)==0){
+            // no data for me, do other tasks:
+            //rprintf("no data found\r\n", plen);
+            continue;
+        }
+        if (buf[IP_PROTO_P]==IP_PROTO_UDP_V&&buf[UDP_DST_PORT_H_P]==(listenUdpPort>>8)&&buf[UDP_DST_PORT_L_P]==(listenUdpPort&0xff)){
+            payloadlen=buf[UDP_LEN_L_P]-UDP_HEADER_LEN;
+
+            char* data = &(buf[UDP_DATA_P]);
+            if(memcmp("co:", data, 3) == 0){
+                rprintf("\r\nco com\r\n", plen);
+            } else if(memcmp("ir:", data, 3) == 0){
+                // example ir:pr=15;ad=00FF;co=1034;fl=00
+                rprintf("\r\nir command\r\n", plen);
+
+                irmp_data.protocol = parseString(data, 6, 2);
+                irmp_data.address = parseString(data, 12, 4);
+                irmp_data.command = parseString(data, 20, 4);
+                irmp_data.flags = parseString(data, 28, 2);
+
+                rprintf("prot:%d, addres:%d, command:%d, flags:%d\r\n", irmp_data.protocol, irmp_data.address,
+                    irmp_data.command, irmp_data.flags);
+
+                uint8_t result = irsnd_send_data (&irmp_data, TRUE);
+                rprintf("ir sending res: %d\r\n", result);
+                if(result == 1){
+                    blink(500);
+                } else {
+                    blink(100);
+                    blink(100);
+                    blink(100);
+                }                
+            } else {
+               rprintf("\r\nerror no command found\r\n", plen); 
+            }
+            make_udp_reply_from_request(buf,data,strlen(data),listenUdpPort);
+        }
     }
     return 0;
 }
--- src/ir_send/ir_send.h
+++ src/ir_send/ir_send.h
@@ -2,6 +2,8 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #include <string.h>
+#include <avr/eeprom.h>
+#include "ip_config.h"
 #include "../common/uart.h"
 #include "../common/rprintf.h"
 #include "../common/ip_arp_udp_tcp.h"
@@ -9,16 +11,20 @@
 #include "../common/timeout.h"
 #include "../common/net.h"
 #include "../common/dhcp_client.h"
-#include "../common/irmp.h"
+#include "../common/irsnd.h"
 #include "../common/timerx8.h"
 
 uint8_t dstip[4]={192,168,100,125};
+// listen port for udp
+uint16_t  listenUdpPort = 6500;
+
+
 
 //
 // Please modify the following lines. mac and ip have to be unique
 // in your local area network. You can not have the same numbers in
 // two devices:
-static uint8_t mymac[6] = {0xc3,0x48,0xa2,0x40,0x45,0x12};
+static uint8_t mymac[6] = {0xa4,0x24,0x65,0x21,0x65,0x73};
 // how did I get the mac addr? Translate the first 3 numbers into ascii is: TUX
 //
 static uint8_t otherside_www_ip[4];
@@ -31,7 +37,7 @@ static uint8_t gwmac[6];
 // Netmask (DHCP will provide a value for it):
 static uint8_t netmask[4];
 // packet buffer
-#define BUFFER_SIZE 50
+#define BUFFER_SIZE 350
 static uint8_t buf[BUFFER_SIZE+1];
 
 // set output to VCC, red LED off
