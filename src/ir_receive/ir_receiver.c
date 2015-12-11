c6e8f4a32738a997006574fe4617f63a6b8bbec0
Jörg Bolay <jorg@bolay.org>
2015-12-11 23:00:15 +0100

    don't know where i am just saving

--- src/ir_receive/ir_receiver.c
+++ src/ir_receive/ir_receiver.c
@@ -19,12 +19,14 @@
 #include "irmp.h"
 #include "timerx8.h"
 
+uint8_t dstip[4]={192,168,100,139};
+
 
 //
 // Please modify the following lines. mac and ip have to be unique
 // in your local area network. You can not have the same numbers in
 // two devices:
-static uint8_t mymac[6] = {0xb4,0x28,0x95,0x23,0x60,0x24};
+static uint8_t mymac[6] = {0xb1,0x38,0x94,0x13,0x50,0x22};
 // how did I get the mac addr? Translate the first 3 numbers into ascii is: TUX
 //
 static uint8_t otherside_www_ip[4];
@@ -40,14 +42,20 @@ static char urlvarstr[30];
 static volatile uint8_t sec=0; // counts up to 6 and goes back to zero
 static volatile uint8_t gsec=0; // counts up beyond 6 sec
 
+
 // set output to VCC, red LED off
-#define LEDOFF PORTD|=(1<<PORTD7)
+#define LED_ON LED_PORT |= _BV(LED_BIT)
 // set output to GND, red LED on
-#define LEDON PORTD&=~(1<<PORTD7)
-// to test the state of the LED
-#define LEDISOFF PORTD&(1<<PORTD7)
+#define LED_OFF LED_PORT &= ~(_BV(LED_BIT))
+#define LED_TOGGLE LED_PORT ^= _BV(LED_BIT)
+#define INIT_LED LED_DDR = _BV (LED_BIT)
+#define LED_PORT PORTC          /* PORTx - register for LED output */
+#define LED_BIT PC4             /* bit for button input/output */
+#define LED_DDR DDRC            /* LED data direction register */
+
+
 // packet buffer
-#define BUFFER_SIZE 650
+#define BUFFER_SIZE 350
 static uint8_t buf[BUFFER_SIZE+1];
 static uint8_t start_web_client=0;
 static uint8_t web_client_attempts=0;
@@ -55,25 +63,33 @@ static int8_t dns_state=0;
 
 uint16_t counter = 0;
 
-
+void blink(uint16_t time){
+    uint16_t count = time;
+    LED_ON;
+    while(count--) {
+        _delay_ms(1);
+    }
+    count = time;
+    LED_OFF;
+    while(count--) {
+        _delay_ms(1);
+    }
+}
 
 void timerFired(){
 	(void) irmp_ISR();
 
 	counter += 1;
-	LEDOFF;
 	if(counter >= 15000){
-		LEDON;
+
 		
 	} else if(counter >= 30000){
-		LEDOFF;
 		counter = 0;
 	}
 }
 
 
 void timer_init(){
-	//timer1Init();
 	timer1CTCInit((F_CPU / F_INTERRUPTS) - 1);
 	timer1SetPrescaler(TIMER_CLK_DIV1);
 	timerAttach(TIMER1OUTCOMPAREA_INT, timerFired);
@@ -89,6 +105,7 @@ void arpresolver_result_callback(uint8_t *ip __attribute__((unused)),uint8_t ref
 }
 
 void get_ip_from_Dhcp(){
+
 	uint8_t rval;
 	uint16_t plen;
 	rval=0;
@@ -103,7 +120,7 @@ void get_ip_from_Dhcp(){
     client_ifconfig(myip,netmask);
     rprintf("myip: %d.%d.%d.%d\r\n",myip[0], myip[1], myip[2], myip[3]);
     //rprintf("gwip: %d.%d.%d.%d\r\n",gwip[0], gwip[1], gwip[2], gwip[3]);
-    LEDOFF;
+    blink(1000);
 
     if (gwip[0]==0){
         // we must have a gateway returned from the dhcp server
@@ -135,25 +152,26 @@ void bufferFunction(char c){
 
 int main (void) {
  
- 	uart_init(UART_BAUD_SELECT(9600,8000000));
+ 	uart_init();
+    timer_init();
+ 	rprintfInit(bufferFunction); 	
+    sei();
+    rprintf("hello world\n");
+    uart_putstr(strBufRprintf);
 
- 	//rprintfInit(uart_putc);
- 	rprintfInit(bufferFunction);
+ 	INIT_LED;
 
- 	uint16_t dat_p;
-    char str[20]; 	
- 	
-
- 	DDRD|= (1<<PD7); // LED, enable PD7 as output
-    LEDON;
+    blink(100);
+    blink(100);
+    blink(100);
+    blink(100);
+    blink(100);
 
 
     /*initialize enc28j60*/
     enc28j60Init(mymac);
     _delay_loop_1(0); // 60us
     
-    timer_init();
-
 	sei();
 
 	/* Magjack leds configuration, see enc28j60 datasheet, page 11 */
@@ -164,21 +182,15 @@ int main (void) {
     enc28j60PhyWrite(PHLCON,0x476);
 
 
-    
-    LEDON;
     // DHCP handling. Get the initial IP
 
-    get_ip_from_Dhcp();
-    
-    LEDON;
-    
+    get_ip_from_Dhcp();    
     get_gateway_mac();
-    LEDOFF;
 
     // send_udp sends via a given gateway. The dstmac is the MAC of the gateway (note this replaces the client_set_gwip() function of previous versions)
 	//extern void send_udp(uint8_t *buf,char *data,uint8_t datalen,uint16_t sport, const uint8_t *dip, uint16_t dport,const uint8_t *dstmac);
 
-    uint8_t dstip[4]={192,168,100,139};
+
 
 	IRMP_DATA   irmp_data;
 	irmp_init();
@@ -186,8 +198,10 @@ int main (void) {
 	//char strBuf[50];
 	//char buf1[4] = "";
 
-	uart_puts(strBufRprintf);
+	uart_putstr(strBufRprintf);
     pointer = 0;
+    blink(200);
+    blink(200);
     while(1) {
 		
 
@@ -195,9 +209,9 @@ int main (void) {
         {
             rprintf("prot:%d;addr:%d;com:%d;flags:%d\n", irmp_data.protocol, irmp_data.address, irmp_data.command, irmp_data.flags);
             send_udp(buf, strBufRprintf, 50, 7001, dstip, 6000, gwmac);
-            uart_puts(strBufRprintf);
+            uart_putstr(strBufRprintf);
             pointer = 0;
-
+            blink(200);
         }
    }  
    return 0;
