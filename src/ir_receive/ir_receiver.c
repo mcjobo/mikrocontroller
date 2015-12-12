e174a463ac36be53ed19e9e20f7207e4a71ea46c
Jörg Bolay <jorg@bolay.org>
2015-12-12 20:26:41 +0100

    fiddleling with MAC adresses

--- src/ir_receive/ir_receiver.c
+++ src/ir_receive/ir_receiver.c
@@ -26,7 +26,8 @@ uint8_t dstip[4]={192,168,100,139};
 // Please modify the following lines. mac and ip have to be unique
 // in your local area network. You can not have the same numbers in
 // two devices:
-static uint8_t mymac[6] = {0xb1,0x38,0x94,0x13,0x50,0x22};
+// beaware not to use a 1 as first bit (like have 0x00 as first)
+static uint8_t mymac[6] = {0x00,0x38,0x94,0x13,0x50,0x55};
 // how did I get the mac addr? Translate the first 3 numbers into ascii is: TUX
 //
 static uint8_t otherside_www_ip[4];
@@ -104,15 +105,44 @@ void arpresolver_result_callback(uint8_t *ip __attribute__((unused)),uint8_t ref
     }
 }
 
+
+
+
+char strBufRprintf[50];
+uint8_t pointer = 0;
+void bufferFunction(char c){
+	strBufRprintf[pointer] = c;
+	++pointer;
+}
+
+void clearBuffer(){
+    uint8_t i;
+    for(; i<50;++i){
+        strBufRprintf[i]=0;
+    }
+    pointer = 0;
+}
+
+
 void get_ip_from_Dhcp(){
 
-	uint8_t rval;
-	uint16_t plen;
-	rval=0;
+    uint8_t rval;
+    uint16_t plen;
+    rval=0;
     init_mac(mymac);
+    
+    rprintf("dhcp stuff: %d\n", enc28j60linkup());
+    uart_putstr(strBufRprintf);
+    
     while(rval==0){
         plen=enc28j60PacketReceive(BUFFER_SIZE, buf);
         buf[BUFFER_SIZE]='\0';
+
+        if(plen > 0){
+            rprintf("dhcp payload length: %d udp port: %d message type %d id_1 %d id_2 %d\n", plen, buf[UDP_SRC_PORT_L_P], buf[UDP_DATA_P], buf[UDP_DATA_P+5], buf[UDP_DATA_P+6]);
+            uart_putstr(strBufRprintf);
+        }   
+
         rval=packetloop_dhcp_initial_ip_assignment(buf,plen,mymac[5]);
     }
     // we have an IP:
@@ -125,14 +155,14 @@ void get_ip_from_Dhcp(){
     if (gwip[0]==0){
         // we must have a gateway returned from the dhcp server
         // otherwise this code will not work
-		uart_putc('g');
+        uart_putc('g');
         while(1); // stop here
     }
 }
  
 void get_gateway_mac(){
-	uint16_t plen;
-	// we have a gateway.
+    uint16_t plen;
+    // we have a gateway.
     // find the mac address of the gateway (e.g your dsl router).
     get_mac_with_arp(gwip,TRANS_NUM_GWMAC,&arpresolver_result_callback);
     while(get_mac_with_arp_wait()){
@@ -142,13 +172,6 @@ void get_gateway_mac(){
     }
 }
 
-
-char strBufRprintf[50];
-uint8_t pointer = 0;
-void bufferFunction(char c){
-	strBufRprintf[pointer] = c;
-	++pointer;
-}
 
 int main (void) {
  
@@ -181,10 +204,12 @@ int main (void) {
     // enc28j60PhyWrite(PHLCON,0b0000 0100 0111 01 10);
     enc28j60PhyWrite(PHLCON,0x476);
 
-
+rprintf("before dhcp\n");
+    uart_putstr(strBufRprintf);
     // DHCP handling. Get the initial IP
-
-    get_ip_from_Dhcp();    
+    get_ip_from_Dhcp();   
+    rprintf("after dhcp\n");
+    uart_putstr(strBufRprintf); 
     get_gateway_mac();
 
     // send_udp sends via a given gateway. The dstmac is the MAC of the gateway (note this replaces the client_set_gwip() function of previous versions)
@@ -202,6 +227,7 @@ int main (void) {
     pointer = 0;
     blink(200);
     blink(200);
+    clearBuffer();
     while(1) {
 		
 
@@ -210,7 +236,7 @@ int main (void) {
             rprintf("prot:%d;addr:%d;com:%d;flags:%d\n", irmp_data.protocol, irmp_data.address, irmp_data.command, irmp_data.flags);
             send_udp(buf, strBufRprintf, 50, 7001, dstip, 6000, gwmac);
             uart_putstr(strBufRprintf);
-            pointer = 0;
+            clearBuffer();
             blink(200);
         }
    }  
